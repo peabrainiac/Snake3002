d49d57398456f5241a83d609948acf9471af87c6
Ben Eltschig <43812953+peabrainiac@users.noreply.github.com>
2020-01-31 18:00:24 +0100

    made renderer a custom element

--- Renderer.mjs
+++ Renderer.mjs
@@ -1,24 +1,61 @@
 import Vao from "./gl/Vao.mjs";
 import ShaderProgram from "./gl/ShaderProgram.mjs";
 
-export default class Snake3002Renderer {
-	constructor(canvas){
-		return (async ()=>{
-			this.canvas = canvas;
-			this.gl = canvas.getContext("webgl2");
+export default class Snake3002Renderer extends HTMLElement {
+	constructor(){
+		super();
+		this._pixelSize = 4;
+		(async ()=>{
+			this.attachShadow({mode:"open"});
+			this.shadowRoot.innerHTML = `
+				<style>
+					:host {
+						display: block;
+						overflow: hidden;
+					}
+					canvas {
+						display: block;
+						image-rendering: -moz-crisp-edges;
+						image-rendering: crisp-edges;
+						image-rendering: pixelated;
+					}
+				</style>
+				<canvas></canvas>
+			`;
+			this.canvas = this.shadowRoot.querySelector("canvas");
+			this.gl = this.canvas.getContext("webgl2");
 			this._vao = new Vao(this.gl);
 			this._vao.addVbo(0,2,[-0.5,-0.5,-0.5,0.5,0.5,-0.5,0.5,-0.5,-0.5,0.5,0.5,0.5]);//[-1,-1,-1,1,1,-1,1,-1,-1,1,1,1]
 			this._shader = new ShaderProgram(this.gl,await (await fetch("./shader.vert")).text(),await (await fetch("./shader.frag")).text());
-			return this;
 		})();
 	}
 
-	render(){
-		this._vao.bind();
-		this.gl.drawArrays(this.gl.TRIANGLES,0,6);
+	update(){
+		let width = this.offsetWidth/this._pixelSize;
+		let height = this.offsetHeight/this._pixelSize;
+		if (this.canvas.width!=width||this.canvas.height!=height){
+			this.canvas.width = width;
+			this.canvas.height = height;
+			this.canvas.style.width = width*this._pixelSize+"px";
+			this.canvas.style.height = height*this._pixelSize+"px";
+		}
+		this.gl.viewport(0,0,width,height);
+		if (this._shader){
+			this._vao.bind();
+			this.gl.drawArrays(this.gl.TRIANGLES,0,6);
+		}
+	}
+
+	get pixelSize(){
+		return this._pixelSize;
+	}
+
+	set pixelSize(pixelSize){
+		this._pixelSize = pixelSize;
 	}
 
 	destroy(){
 		this._vao.destroy();
 	}
 }
+window.customElements.define("snake-3002-renderer",Snake3002Renderer);
--- Snake3002.mjs
+++ Snake3002.mjs
@@ -1,33 +1,27 @@
-import Snake3002Renderer from "./Renderer.mjs";
+import "./Renderer.mjs";
 
 export default class Snake3002 extends HTMLElement {
 	constructor(){
 		super();
-		(async ()=>{
-			this.attachShadow({mode:"open"});
-			this.shadowRoot.innerHTML = `
-				<style>
-					:host {
-						display: block;
-					}
-					#canvas {
-						display: block;
-						width: 100%;
-						height: 100%;
-						image-rendering: -moz-crisp-edges;
-						image-rendering: crisp-edges;
-						image-rendering: pixelated;
-					}
-				</style>
-				<canvas id="canvas"></canvas>
-			`;
-			this.renderer = await new Snake3002Renderer(this.shadowRoot.getElementById("canvas"));
-			this.update();
-		})();
+		this.attachShadow({mode:"open"});
+		this.shadowRoot.innerHTML = `
+			<style>
+				:host {
+					display: block;
+				}
+				snake-3002-renderer {
+					width: 100%;
+					height: 100%;
+				}
+			</style>
+			<snake-3002-renderer></snake-3002-renderer>
+		`;
+		this.renderer = this.shadowRoot.querySelector("snake-3002-renderer");
+		this.update();
 	}
 
 	update(){
-		this.renderer.render();
+		this.renderer.update();
 		requestAnimationFrame(()=>{
 			this.update();
 		});
