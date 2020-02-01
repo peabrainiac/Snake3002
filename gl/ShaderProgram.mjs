1c261f78d9ffb76880e93da6920d2bd7e30c502d
Ben Eltschig <43812953+peabrainiac@users.noreply.github.com>
2020-02-01 18:03:02 +0100

    improved uniform type handling

--- gl/ShaderProgram.mjs
+++ gl/ShaderProgram.mjs
@@ -11,6 +11,7 @@ export default class ShaderProgram {
 		gl.attachShader(this._id,this._vertexShader.id);
 		gl.attachShader(this._id,this._fragmentShader.id);
 		this._uniformLocations = {};
+		this._uniformTypes = {};
 		this.uniforms = new Proxy({},{set:(target,property,value,receiver)=>{
 			target[property] = value;
 			if (this._isReady){
@@ -36,6 +37,19 @@ export default class ShaderProgram {
 		}
 		this._gl.useProgram(this._id);
 		this._uniformLocations = {};
+		this._uniformTypes = {};
+		this._uniformNames = {};
+		let count = this._gl.getProgramParameter(this._id,this._gl.ACTIVE_UNIFORMS);
+		let infos = [];
+		for (let i=0;i<count;i++){
+			let info = this._gl.getActiveUniform(this._id,i);
+			let name = info.name.match(/^[^[]*/);
+			this._uniformTypes[name] = info.type;
+			this._uniformNames[name] = info.name;
+			this._uniformLocations[name] = this._gl.getUniformLocation(this._id,name);
+			infos.push(info);
+		}
+		console.log("Active Uniforms:",infos);
 		let uniforms = Object.getOwnPropertyNames(this.uniforms);
 		for (let i=0;i<uniforms.length;i++){
 			this.load(uniforms[i],this.uniforms[uniforms[i]]);
@@ -47,33 +61,47 @@ export default class ShaderProgram {
 	}
 
 	load(name,value){
-		if (value instanceof Matrix3f){
-			this.loadMatrix3f(name,value);
-		}else if(value instanceof Vector3f){
-			this.loadVector3f(name,value);
-		}else{
+		let type = this._uniformTypes[name];
+		if(!this._uniformLocations[name]){
+			console.warn("Error loading uniform! Name:",name,", type:",type,", value:",value);
+		}else if (type==this._gl.INT){
+			this.loadInt(name,value);
+		}else if(type==this._gl.FLOAT){
 			this.loadFloat(name,value);
+		}else if(type==this._gl.FLOAT_VEC3){
+			if (this._uniformNames[name].endsWith("[0]")){
+				this.loadVector3fArray(name,value);
+			}else{
+				this.loadVector3f(name,value);
+			}
+		}else if(type==this._gl.FLOAT_MAT3){
+			this.loadMatrix3f(name,value);
 		}
 	}
 
+	loadInt(name,value){
+		this._gl.uniform1i(this._uniformLocations[name],value);
+	}
+
 	loadFloat(name,value){
-		if (!this._uniformLocations[name]){
-			this._uniformLocations[name] = this._gl.getUniformLocation(this._id,name);
-		}
 		this._gl.uniform1f(this._uniformLocations[name],value);
 	}
 
 	loadVector3f(name,vector){
-		if (!this._uniformLocations[name]){
-			this._uniformLocations[name] = this._gl.getUniformLocation(this._id,name);
-		}
 		this._gl.uniform3f(this._uniformLocations[name],vector.x,vector.y,vector.z);
 	}
 
 	loadMatrix3f(name,matrix){
-		if (!this._uniformLocations[name]){
-			this._uniformLocations[name] = this._gl.getUniformLocation(this._id,name);
-		}
 		this._gl.uniformMatrix3fv(this._uniformLocations[name],false,matrix.toArray());
+	}
+
+	loadVector3fArray(name,vectors){
+		let array = new Float32Array(3*vectors.length);
+		for (let i=0;i<vectors.length;i++){
+			array[3*i] = vectors[i].x;
+			array[3*i+1] = vectors[i].y;
+			array[3*i+2] = vectors[i].z;
+		}
+		this._gl.uniform3fv(this._uniformLocations[name],array);
 	}
 }
